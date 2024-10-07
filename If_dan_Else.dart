b30a6bc7543df9054bce50d7327f0364394c8aff
Arifin-only <arifin180104@gmail.com>
2024-10-07 20:42:53 +0800

    Create If_dan_Else.dart

---
+++ If_dan_Else.dart
@@ -0,0 +1,10 @@
+ void main(){
+   var nilaiTugas = 70;
+   var nilaiAbsen = 80;
+
+   if(nilaiTugas >= 75 && nilaiAbsen >= 75){
+     print('kamu berhasil ges');
+   }else{
+     print('yuyyy ngulang');
+   }
+ 
